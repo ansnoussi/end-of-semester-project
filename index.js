c1656398335c6725b756d0c822f2468fb73ee56c
Anis Snoussi <snoussi.anis@insat.u-carthage.tn>
2022-01-17 11:23:54 +0100

    some comments

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ const client = require("prom-client")
 const {encode, decode} = require('./utils')
 
 
+// ******************************************************
+// **********************App config**********************
+// ******************************************************
+
+// Express app
 const app = express();
 app.use(cors());
 
+// Collect health metrics every 5s
 const collectDefaultMetrics = client.collectDefaultMetrics
 collectDefaultMetrics({timeout : 5000})
 
+// Custom metrics for total requests in app
 const counterEnc = new client.Counter({
   name: "node_encode_request_total",
   help: "The total number of encode requests"
@@ -20,8 +27,11 @@ const counterDec = new client.Counter({
   help: "The total number of decode requests"
 })
 
+// ******************************************************
+// ***************functional endpoints*******************
+// ******************************************************
 
-
+// Encode string to base64
 app.get("/encode/:str", (req, res) => {
   try {
     
@@ -36,6 +46,7 @@ app.get("/encode/:str", (req, res) => {
   }
 });
 
+// Decode string from base64
 app.get("/decode/:str", (req, res) => {
   try {
 
@@ -52,19 +63,28 @@ app.get("/decode/:str", (req, res) => {
 
 
 
+// ******************************************************
+// ***************additional endpoints*******************
+// ******************************************************
+
+// Healthcheck enpoint
 app.get("/ping", (req, res) => {
   return res.status(200).send("pong");
 });
 
-
+// Metrics endpoint
 app.get("/metrics", async (req, res) => {
   res.set("Content-Type", client.register.contentType)
   res.end( await client.register.metrics())
 });
 
 
+// ******************************************************
+// **************server execution config*****************
+// ******************************************************
+
 var port = process.env.APP_PORT || 3000;
 
 app.listen(port, () => {
-  console.log("Microservice started.");
+  console.log("App started.");
 });
