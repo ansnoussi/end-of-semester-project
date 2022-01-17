1b379635e2a40e99a218490e7197ffcb0dc4a501
Anis Snoussi <snoussi.anis@insat.u-carthage.tn>
2022-01-17 11:13:46 +0100

    metrics endpoint

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const express = require("express");
 const cors = require("cors");
+const client = require("prom-client")
 const {encode, decode} = require('./utils')
 
 
 const app = express();
 app.use(cors());
 
+const collectDefaultMetrics = client.collectDefaultMetrics
+collectDefaultMetrics({timeout : 5000})
 
-app.get("/ping", (req, res) => {
-  return res.status(200).send("pong");
-});
+const counterEnc = new client.Counter({
+  name: "node_encode_request_total",
+  help: "The total number of encode requests"
+})
+
+const counterDec = new client.Counter({
+  name: "node_decode_request_total",
+  help: "The total number of decode requests"
+})
+
+
 
 app.get("/encode/:str", (req, res) => {
   try {
     
     let str = req.params.str;
-    
+    counterEnc.inc()
     return res.status(200).json({
       encoded: encode(str),
     });
@@ -29,7 +40,7 @@ app.get("/decode/:str", (req, res) => {
   try {
 
     let str = req.params.str;
-    
+    counterDec.inc()
     return res.status(200).json({
       encoded: decode(str),
     });
@@ -39,6 +50,17 @@ app.get("/decode/:str", (req, res) => {
   }
 });
 
+
+
+app.get("/ping", (req, res) => {
+  return res.status(200).send("pong");
+});
+
+
+app.get("/metrics", (req, res) => {
+  res.set("Content-Type", client.register.contentType)
+  res.end(client.register.metrics())
+});
 
 
 var port = process.env.APP_PORT || 3000;
